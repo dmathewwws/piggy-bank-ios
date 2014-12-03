25a0b094febee8b6a37b6faa0abc40200161008c
dannylmathews <dannyl.mathews@gmail.com>
2014-12-03 13:00:38 -0800

    Initial Commit

---
+++ piggy-bank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  piggy-bank
+//
+//  Created by Daniel Mathews on 2014-12-03.
+//  Copyright (c) 2014 com.red-cedar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
