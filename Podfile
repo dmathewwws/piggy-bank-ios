6587598bbf95b0f575b84020109e344e8818b0c0
dannylmathews <dannyl.mathews@gmail.com>
2014-12-18 15:29:23 -0800

    starting StrongLoop verfirication

---
+++ Podfile
@@ -0,0 +1,3 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, "8.0"
+pod 'libPhoneNumber-iOS', '~> 0.7'
--- piggy-bank/ViewController.swift
+++ piggy-bank/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 import MessageUI
 
-class ViewController: UIViewController, MFMessageComposeViewControllerDelegate  {
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate, UITextFieldDelegate  {
+    
+    @IBOutlet weak var verifyLabel: UILabel!
+    
     
     
     override func viewDidLoad() {
@@ -26,14 +29,18 @@ class ViewController: UIViewController, MFMessageComposeViewControllerDelegate  
     
     @IBAction func sendSMS(sender: AnyObject) {
         
-        let sendSMSController = MFMessageComposeViewController()
-        
-        sendSMSController.body = "This is a test!"
-        sendSMSController.recipients = ["1-604-337-1167"]
-        sendSMSController.messageComposeDelegate = self
-        
-        self.presentViewController(sendSMSController, animated: true, completion: nil)
-        
+        if(MFMessageComposeViewController.canSendText()){
+            
+            verifyLabel.text = ""
+            
+            let sendSMSController = MFMessageComposeViewController()
+            
+            sendSMSController.body = "With verification code: XXXX-YYYY-ZZZZ"
+            sendSMSController.recipients = ["1-604-337-1167"]
+            sendSMSController.messageComposeDelegate = self
+            
+            self.presentViewController(sendSMSController, animated: true, completion: nil)
+        }
     }
     
     func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
@@ -41,13 +48,19 @@ class ViewController: UIViewController, MFMessageComposeViewControllerDelegate  
         switch result.value{
             case MessageComposeResultCancelled.value, MessageComposeResultFailed.value:
                 self.dismissViewControllerAnimated(true, completion: {
-                    let alertController = UIAlertController(title: "Sorry!", message: "The text message could not be sent. Please try again at a later time.", preferredStyle: .Alert)
-                    alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-                    self.presentViewController(alertController, animated: true, completion: nil)
+                    
+                    self.verifyLabel.text = "Sorry! The text message could not be sent. Please try again at a later time."
+                    
+//                    let alertController = UIAlertController(title: "Sorry!", message: "The text message could not be sent. Please try again at a later time.", preferredStyle: .Alert)
+//                    alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+//                    self.presentViewController(alertController, animated: true, completion: nil)
                 })
             case MessageComposeResultSent.value:
+                
+                println(result)
+                
                 self.dismissViewControllerAnimated(true, completion: {
-                    let successViewController:SMSSuccesViewController = UIStoryboard(name: "MainApp", bundle:NSBundle.mainBundle()).instantiateViewControllerWithIdentifier("SMSSuccesViewController") as SMSSuccesViewController
+                    let successViewController = UIStoryboard(name: "MainApp", bundle:NSBundle.mainBundle()).instantiateViewControllerWithIdentifier("SMSSuccesViewController") as SMSSuccesViewController
                     self.presentViewController(successViewController, animated: true, completion: nil)
                 })
             default:
